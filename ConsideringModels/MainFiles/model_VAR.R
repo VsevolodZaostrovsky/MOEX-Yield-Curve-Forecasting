f3ec9b46d24982f3ede95635e45967b00958f1d3
Cloda <icherepaxin@bk.ru>
2023-12-03 23:32:08 +0300

    add: test Granger's

--- ConsideringModels/MainFiles/model_VAR.R
+++ ConsideringModels/MainFiles/model_VAR.R
@@ -1,11 +1,13 @@
 # LIBRARY IMPORT
 
+graphics.off()
+
 library(forecast)
 library(tseries)
 library(ggplot2)
 library(Metrics)
-library(xts)
-library(seasonal)
+library(vars)
+library(urca)
 
 
 # --------------------
@@ -14,6 +16,9 @@ library(seasonal)
 
 dataset <- read_csv("FinancialEconometrics/General_ds_cleaned.csv",
                     col_types = cols(date = col_date(format = "%Y-%m-%d")))
+
+dates_from_struct_ms <- c(657, 1384, 3943, 4502)
+horizon_for_forecast <- 10
 
 dataset_yield <- data.frame(
                             month3 = exp(diff(log(dataset$"0.25"))) - 1,
@@ -30,4 +35,22 @@ dataset_yield <- data.frame(
                             year30 = exp(diff(log(dataset$"30.0"))) - 1)
 
 
+train_data <- dataset_yield[dates_from_struct_ms[1]:dates_from_struct_ms[2], ]
+
+# 1) Посмотреть тест Грейнжера(причинность)
+# 2) Еще раз проверить коинтеграцию
+# 3) Найти лучшую модель по AIC
+# 4) Проверить корреляцию остатков с помощью статистики Дербина
+# 5) Обучиться на лучшей моделе
+
+
+
+# пройдемся по лагам и посмотрим на AIC(макс = 4)
+m <- VARselect(train_data, lag.max = 10, type = "const")
+m$selection
+# получаем, что лучший AIC на 5 лагах
+
+var_model_to_fit <- VAR(train_data, p = 5, type = "const")
+var_model_predict <- predict(var_model_to_fit, n.ahead = horizon_for_forecast)
+var_model_predict$fc
 
